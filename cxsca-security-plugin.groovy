9438cbb9948aa2b25dd5b939a80148e9db9fa9db
happy yang <yang2149@163.com>
2023-09-21 19:01:49 +0800

    [UPDATE] groovy file

--- cxsca-security-plugin.groovy
+++ cxsca-security-plugin.groovy
@@ -41,12 +41,15 @@ upload {
     }
 }
 
-// curl -i -u admin:password -X POST "http://localhost:8082/artifactory/api/plugins/execute/scaScanCtl?params=searchPattern=*.whl;repos=pypi-remote-cache;forceScan=true"
+// curl -i -u admin:password -X POST "http://localhost:8082/artifactory/api/plugins/execute/scaScanCtl?params=excludePattern=com.coupang*;numberOfThreads=4;searchPattern=*.whl;repos=pypi-remote-cache;forceScan=true"
 
 def pluginGroup = 'cleaners'
 executions {
     scaScanCtl(groups: [pluginGroup]) { params ->
+        def excludePattern = params['excludePattern'] ? params['excludePattern'][0] as String : ''
+        def numberOfThreads = params['numberOfThreads'] ? params['numberOfThreads'][0] as int : ''
         def searchPattern = params['searchPattern'] ? params['searchPattern'][0] as String : ''
+
         log.info "searchPattern:  $searchPattern"
         // Ensure the given repo list is not empty
         def repos = params?.get('repos')
@@ -55,23 +58,40 @@ executions {
             status = 400
             return
         }
-
         log.info "repos:  $repos"
-
         def forceScan = params['forceScan'] ? params['forceScan'][0] as Boolean : false
         log.info "forceScan:  $forceScan"
 
         log.info("Execute Sca Scan Start")
         def artifacts = searches.artifactsByName(searchPattern, repos.toArray(String[]::new))
-        def index = 0g
-        for (artifact in artifacts ) {
-            log.info "total artifacts number: $artifacts.size"
-            log.info "index: $index"
-            def artifactRepoPath = artifact.toPath()
-            log.info "artifact repo path: $artifactRepoPath"
-            scaPlugin.checkArtifactsAlreadyPresent(artifact, forceScan)
-            index++
+        def artifactsSubLists = artifacts.collate(numberOfThreads);
+        def excludeArtifacts = searches.artifactsByName(excludePattern, repos.toArray(String[]::new))
+        def excludeArtifactsRepoPathList = []
+        excludeArtifacts.each { excludedArtifact ->
+            def repoPath = excludedArtifact.toPath()
+            excludeArtifactsRepoPathList << repoPath
         }
+
+        def requestThreads = []
+        artifactsSubLists.eachWithIndex { artifactsSubList, threadIndex ->
+            Thread requestThread = new Thread({
+                artifactsSubList.eachWithIndex { artifact, index ->
+                    log.info "Thread index: $threadIndex"
+                    log.info "total artifacts number: $artifactsSubList.size"
+                    log.info "index: $index"
+                    def artifactRepoPath = artifact.toPath()
+                    log.info "artifact repo path: $artifactRepoPath"
+                    if (excludeArtifactsRepoPathList.contains(artifactRepoPath)) {
+                        return
+                    }
+                    scaPlugin.checkArtifactsAlreadyPresent(artifact, forceScan)
+                }
+            })
+            requestThreads << requestThread
+        }
+        requestThreads.each { it.start() }
+        requestThreads.each { it.join() }
+
         log.info("Execute Sca Scan Completed")
     }
 }
